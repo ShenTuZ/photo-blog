927b3f1caa24c7bdbc061fbf8ebcdb32e0245dda
ShenTuZi <3125478624@qq.com>
2026-06-05 15:39:41 +0800

    fix: API 改回 ES 模块语法，修复 require 报错

--- api/like.js
+++ api/like.js
@@ -1,8 +1,8 @@
-const { neon } = require('@neondatabase/serverless')
+import { neon } from '@neondatabase/serverless'
 
 const sql = neon(process.env.DATABASE_URL)
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
